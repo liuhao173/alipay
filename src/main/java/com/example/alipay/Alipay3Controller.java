988c25a80ee07825769e32920bca8c6314186057
liuhao173 <liuhaojava.net28>
2021-10-27 20:35:52 +0800

    0

--- src/main/java/com/example/alipay/Alipay3Controller.java
+++ src/main/java/com/example/alipay/Alipay3Controller.java
@@ -1,13 +1,17 @@
 package com.example.alipay;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alipay.api.AlipayClient;
 import com.alipay.api.domain.AlipayTradeCancelModel;
 import com.alipay.api.domain.AlipayTradePrecreateModel;
+import com.alipay.api.domain.AlipayTradeQueryModel;
 import com.alipay.api.internal.util.AlipaySignature;
 import com.alipay.api.request.AlipayTradeCancelRequest;
 import com.alipay.api.request.AlipayTradePrecreateRequest;
+import com.alipay.api.request.AlipayTradeQueryRequest;
 import com.alipay.api.response.AlipayTradeCancelResponse;
 import com.alipay.api.response.AlipayTradePrecreateResponse;
+import com.alipay.api.response.AlipayTradeQueryResponse;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
@@ -36,41 +40,69 @@ public class Alipay3Controller {
     private AlipayProperties alipayProperties;
 
     @RequestMapping("/precreate")
-    public void precreate(HttpServletResponse response) throws Exception{  //商户预创建支付订单，生成二维码
-        AlipayTradePrecreateModel model = new AlipayTradePrecreateModel();
-        model.setOutTradeNo("666");                   //商户订单号
-        model.setProductCode("FACE_TO_FACE_PAYMENT"); //销售产品码
-        model.setSubject("易政通商城产品");                    //订单标题
-        model.setTotalAmount("100");                  //订单总金额
-
+    public void precreate(HttpServletResponse response) throws Exception{
         AlipayTradePrecreateRequest request = new AlipayTradePrecreateRequest();
+        request.setNotifyUrl(alipayProperties.getNotifyUrl());
+        request.setReturnUrl(alipayProperties.getReturnUrl());
+
+        /**
+        AlipayTradePrecreateModel model = new AlipayTradePrecreateModel();
+        model.setOutTradeNo(OrderCodeFactory.getOrderCode(100L));
+        model.setSubject("打赏");
+        model.setTotalAmount("0.01");
         request.setBizModel(model);
+        */
+        JSONObject bizContent = new JSONObject();
+        bizContent.put("out_trade_no", OrderCodeFactory.getOrderCode(100L));
+        bizContent.put("total_amount", 0.01);
+        bizContent.put("subject", "打赏");
+        request.setBizContent(bizContent.toString());
 
         AlipayTradePrecreateResponse alipayTradePrecreateResponse = alipayClient.execute(request);
-        String content=alipayTradePrecreateResponse.getQrCode();
-
-        makeQRCode(content,response.getOutputStream());
+        if(alipayTradePrecreateResponse.isSuccess()){
+            System.out.println("调用成功：" + JSONObject.toJSONString(alipayTradePrecreateResponse));
+            makeQRCode(alipayTradePrecreateResponse.getQrCode(), response.getOutputStream());
+        } else {
+            System.out.println("调用失败");
+        }
     }
 
-    public void makeQRCode(String content, OutputStream outputStream) throws Exception{  //生成二维码
+    public void makeQRCode(String content, OutputStream outputStream) throws Exception{
         Map<EncodeHintType,Object> map = new HashMap<>();
         map.put(EncodeHintType.CHARACTER_SET, StandardCharsets.UTF_8);
         map.put(EncodeHintType.MARGIN,2);
-
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
         BitMatrix bitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE,200,200,map);
         MatrixToImageWriter.writeToStream(bitMatrix,"jpeg",outputStream);
     }
 
     @RequestMapping("/cancel/{code}")
-    public String cancel(@PathVariable("code") String code) throws Exception{  //取消订单，支付超时、支付结果未知是可撤销，超过24小时不可撤销
+    public String cancel(@PathVariable("code") String code) throws Exception{
+        AlipayTradeCancelRequest request = new AlipayTradeCancelRequest();
+        /**
         AlipayTradeCancelModel model = new AlipayTradeCancelModel();
         model.setOutTradeNo(code);
-
-        AlipayTradeCancelRequest request = new AlipayTradeCancelRequest();
         request.setBizModel(model);
-
+        */
+        JSONObject bizContent = new JSONObject();
+        bizContent.put("out_trade_no", code);
+        request.setBizContent(bizContent.toString());
         AlipayTradeCancelResponse response = alipayClient.execute(request);
+        return response.getBody();
+    }
+
+    @RequestMapping("/query/{code}")
+    public String query(@PathVariable("code") String code) throws Exception{
+        AlipayTradeQueryRequest request = new AlipayTradeQueryRequest();
+        /**
+        AlipayTradeQueryModel model = new AlipayTradeQueryModel();
+        model.setOutTradeNo(code);
+        request.setBizModel(model);
+        */
+        JSONObject bizContent = new JSONObject();
+        bizContent.put("out_trade_no", code);
+        request.setBizContent(bizContent.toString());
+        AlipayTradeQueryResponse response = alipayClient.execute(request);
         return response.getBody();
     }
 
@@ -78,7 +110,7 @@ public class Alipay3Controller {
     public void notify(HttpServletRequest request) throws Exception{   //trade_success状态下异步通知接口
         if (check(request.getParameterMap())){
             System.out.println(request.getParameter("trade_status"));
-            System.out.println("异步通知 "+ Instant.now());
+            System.out.println("异步通知：" + Instant.now());
         }else {
             System.out.println("验签失败");
         }
@@ -95,18 +127,15 @@ public class Alipay3Controller {
 
     private boolean check(Map<String,String[]> requestParams) throws Exception{  //对return、notify参数进行验签
         Map<String,String> params = new HashMap<>();
-
         for (String name : requestParams.keySet()) {
             String[] values = requestParams.get(name);
             String valueStr = "";
             for (int i = 0; i < values.length; i++) {
                 valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
             }
-
             params.put(name, valueStr);
             System.out.println(name + " ==> " + valueStr);
         }
-
-        return AlipaySignature.rsaCheckV1(params, alipayProperties.getAlipayPublicKey(), alipayProperties.getCharset(), alipayProperties.getSignType()); //调用SDK验证签名
+        return AlipaySignature.rsaCheckV1(params, alipayProperties.getAlipayPublicKey(), alipayProperties.getCharset(), alipayProperties.getSignType());
     }
 }
